c6d5e49d83801dba4572d8c31e09e46cd4841ef5
Arlly Cavalcanti <arllyepc@gmail.com>
2023-04-10 17:20:13 -0300

    hero

---
+++ js/all.js
@@ -0,0 +1,1 @@
+"use strict";var slide_hero=new Swiper(".slide-principal",{effect:"fade",autoplay:{delay:5e3,disableOnInteraction:!1}});
---
+++ js/scripts/main.js
@@ -0,0 +1,7 @@
+var slide_hero = new Swiper('.slide-principal', {
+	effect: 'fade',
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+});
